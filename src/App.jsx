d487cf7886a6d89044908424e75fbb11921398fd
srinivaspokala777 <srinivaspokala777@gmail.com>
2026-02-09 12:38:47 +0530

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ const App = () => {
   const [tooglePassword, setTooglePassword] = useState("password");
   const [toogleConfirmPassword, setToogleConfirmPassword] = useState("password");
  const[isLoginview,setIsLoginview]=useState(true);
+ const[formData,setFormData]=useState({email:"",password:"",cpassword:"",mobile:"",fullName:""  });
+ const[errorData,setErrorData]=useState({email:"",password:"",cpassword:"",mobile:"",fullName:""});
   function tooglePwd(){
     setTooglePassword(tooglePassword === "password" ? "text" : "password");
   }
@@ -14,9 +16,54 @@ const App = () => {
     setToogleConfirmPassword(toogleConfirmPassword === "password" ? "text" : "password");
   }
   function switchview(){
+    setFormData({fullName:"",email:"",mobile:"",password:"",cpassword:""})
     setIsLoginview(isLoginview==true? false:true);
   }
-
+  function handleInput(e){
+    const{name,value}=e.target;
+    setFormData({...formData,[name]:value});
+  }
+  function signin(){
+    if(validatesignin())
+      return;
+      alert("Login Successfull");
+  }
+  function validatesignin(){
+    let errors={};
+    if (formData.email.trim()===""){
+      errors.email=true;
+      if(formData.password.trim()===""){
+        errors.password=true;
+        setErrorData(errors);
+        return object.keys(errors).length > 0;
+      }
+    }
+  }
+  function signup(){
+    if(validatesignup())
+      return;
+    alert("Registered Successfully");
+  }
+  function validatesignup(){
+    let errors={};
+    if (formData.fullName.trim()===""){
+      errors.fullName=true;
+    }
+    if (formData.email.trim()===""){
+      errors.email=true;
+    }
+    if (formData.mobile.trim()===""){
+      errors.mobile=true;
+    }
+    if (formData.password.trim()===""){
+      errors.password=true;
+    }
+    if (formData.cpassword.trim()==="" || formData.cpassword!==formData.password){
+      errors.cpassword=true;
+    }
+    setErrorData(errors);
+    return Object.keys(errors).length > 0;
+  }
   return (
     <div className='app'>
       {isLoginview ==true &&
@@ -24,15 +71,15 @@ const App = () => {
         <h2>Sign in with email</h2>
         <div className='input-group'>
           <img className='left-icon' src={IMGURL + "email.png"} alt='' />
-          <input type='text' placeholder='Enter your email' />
+          <input type='text' className={errorData.email? "error":" "} placeholder='Enter your email' name='email' value={formData.email} onChange={(e)=>handleInput(e)}/>
         </div>
         <div className='input-group'>
           <img className='left-icon' src={IMGURL + "password.png"} alt='' />
-          <input type={tooglePassword} placeholder='Enter your password' />
+          <input type={tooglePassword} className={errorData.password? "error":" "} placeholder='Enter your password'  name='password'  value={formData.password} onChange={(e)=>handleInput(e)}/>
           <img className='right-icon' src={IMGURL + "eye.png"} alt='' onClick={()=>tooglePwd()} />
         </div>
         <div className='forgot-password'>Forgot <label>Password</label>?</div>
-        <button>Get Started</button>
+        <button onClick={()=>signin()}>Get Started</button>
         <p>Don't have an account? <label onClick={()=>switchview()}>Sign up</label></p>
       </div>
   }
@@ -41,33 +88,31 @@ const App = () => {
         <h2>Create Account</h2>
         <div className='input-group'>
           <img className='left-icon' src={IMGURL + "user.png"} alt='' />
-          <input type='text' placeholder='Full Name' />
+          <input type='text' className={errorData.fullName ? "error":""} placeholder='Full Name' value={formData.fullName} onChange={(e)=>handleInput(e)}/>
         </div>
         <div className='input-group'>
           <img className='left-icon' src={IMGURL + "email.png"} alt='' />
-          <input type='text' placeholder='Email' />
+          <input type='text' className={errorData.email ? "error":""} placeholder='Email' value={formData.email} onChange={(e)=>handleInput(e)} />
         </div>
         <div className='input-group'>
           <img className='left-icon' src={IMGURL + "telephone.png"} alt='' />
-          <input type='text' placeholder='Mobile Number' />
+          <input type='text' className={errorData.mobile ? "error":""} placeholder='Mobile Number'  value={formData.mobile} onChange={(e)=>handleInput(e)}/>
         </div>
         <div className='input-group'>
           <img className='left-icon' src={IMGURL + "password.png"} alt='' />
-          <input type={tooglePassword} placeholder='Password' />
+          <input type={tooglePassword} className={errorData.password ? "error":""} placeholder='Password'  value={formData.password} onChange={(e)=>handleInput(e)}/>
           <img className='right-icon' src={IMGURL + "eye.png"} alt='' onClick={()=>tooglePwd()} />
         </div>
         <div className='input-group'>
           <img className='left-icon' src={IMGURL + "password.png"} alt='' />
-          <input type={toogleConfirmPassword} placeholder='Confirm Password' />
+          <input type={toogleConfirmPassword} className={errorData.cpassword ? "error":""} placeholder='Confirm Password' value={formData.cpassword} onChange={(e)=>handleInput(e)} />
           <img className='right-icon' src={IMGURL + "eye.png"} alt='' onClick={()=>toogleConfirmPwd()} />
         </div>
-        <button>Register</button>
+        <button onClick={()=>signup()}>Register</button>
         <p>Already have an account? <label onClick={()=>switchview()}>Login Here</label></p>
       </div>
-}
+    }
     </div>
-  
   );
 }
-
 export default App;
